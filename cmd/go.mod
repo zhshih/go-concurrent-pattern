e5c259ff698148806f36cfa46542574aeada9180
Z.H <galaxyshih@gmail.com>
2024-04-24 23:27:03 +0800

    Enrich demo example

---
+++ cmd/go.mod
@@ -0,0 +1,13 @@
+module github.com/zhshih/go-concurrent-pattern
+
+go 1.21.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.1
+	github.com/temoto/robotstxt v1.1.2
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.21.0 // indirect
+)
--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,136 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"regexp"
+	"runtime"
+	"strings"
 	"sync"
+	"time"
+
+	"github.com/temoto/robotstxt"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+const (
+	maxURL = 1000
+	delay  = 300 * time.Millisecond
+)
+
+type Article struct {
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Date     string `json:"date"`
+	Category string `json:"category"`
+	Content  string `json:"content"`
+}
+
+func extractURLs(html string) []string {
+	re := regexp.MustCompile(`href=["'](https?://[^"']+)["']`)
+	matches := re.FindAllStringSubmatch(html, -1)
+	urls := make([]string, len(matches))
+	for i, match := range matches {
+		urls[i] = match[1]
+	}
+	return urls
+}
+
 // Generator pattern
-func generateURLs(urls ...string) <-chan string {
+func generateURLs(entryURLs ...string) <-chan string {
 	out := make(chan string)
 	go func() {
 		defer close(out)
-		for _, url := range urls {
-			out <- url
+		seen := make(map[string]bool)
+		var count int
+		var generate func(url string)
+		generate = func(url string) {
+			if count >= maxURL {
+				return
+			}
+			if seen[url] {
+				return
+			}
+			seen[url] = true
+			count++
+
+			resp, err := http.Get(url + "/robots.txt")
+			if err != nil {
+				log.Println("error fetching robots.txt:", err)
+				return
+			}
+			defer resp.Body.Close()
+			data, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("error reading robots.txt:", err)
+				return
+			}
+			robots, err := robotstxt.FromBytes(data)
+			if err != nil {
+				log.Println("error parsing robots.txt:", err)
+				return
+			}
+			if !robots.TestAgent(url, "Test123") {
+				log.Println("URL disallowed by robots.txt:", url)
+				return
+			}
+
+			resp, err = http.Get(url)
+			if err != nil {
+				log.Println("error fetching URL:", err)
+				return
+			}
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("error reading body:", err)
+				return
+			}
+			for _, url := range extractURLs(string(body)) {
+				if count >= maxURL {
+					log.Println("stop generating URL due to exceeding max count")
+					return
+				}
+				log.Printf("generate URL = %s", url)
+				out <- url
+
+				time.Sleep(delay)
+
+				generate(url)
+			}
+		}
+		for _, entryURL := range entryURLs {
+			generate(entryURL)
 		}
 	}()
 	return out
 }
 
 // Fan-out pattern
-func fetchURL(url <-chan string) <-chan string {
+func fetchURL(urls <-chan string) <-chan string {
 	out := make(chan string)
 	go func() {
 		defer close(out)
-		for u := range url {
-			resp, err := http.Get(u)
+		for url := range urls {
+			log.Printf("fetch URL = %s", url)
+			resp, err := http.Get(url)
 			if err != nil {
-				fmt.Println("Error:", err)
+				log.Println("error fetching URL:", err)
 				continue
 			}
-			defer resp.Body.Close()
 			body, err := io.ReadAll(resp.Body)
+			resp.Body.Close()
 			if err != nil {
-				fmt.Println("Error:", err)
+				log.Println("error reading body:", err)
 				continue
 			}
 			out <- string(body)
 		}
+		close(out)
 	}()
 	return out
 }
@@ -73,7 +166,33 @@ func processHTML(html <-chan string) <-chan string {
 	go func() {
 		defer close(out)
 		for h := range html {
-			out <- "Processed HTML: " + h[:50] // Just for demonstration
+			doc, err := goquery.NewDocumentFromReader(strings.NewReader(h))
+			if err != nil {
+				log.Println("error parsing HTML:", err)
+				continue
+			}
+
+			title := doc.Find("h1").Text()
+			author := doc.Find(".author").Text()
+			date := doc.Find(".date").Text()
+			category := doc.Find(".category").Text()
+			content := doc.Find(".content").Text()
+			if len(content) > 50 {
+				content = content[:50] + "...(remaining content has been omitted for brevity)"
+			}
+			article := Article{
+				Title:    title,
+				Author:   author,
+				Date:     date,
+				Category: category,
+				Content:  content,
+			}
+			jsonStr, err := json.Marshal(article)
+			if err != nil {
+				log.Println("error marshal article:", err)
+			}
+			processedHTML := fmt.Sprintf("processed HTML = %s", string(jsonStr))
+			out <- processedHTML
 		}
 	}()
 	return out
@@ -88,8 +207,9 @@ func workerPool(in <-chan string, numWorkers int) <-chan string {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for url := range in {
-				out <- url
+			for html := range in {
+				processedHTML := html
+				out <- processedHTML
 			}
 		}()
 	}
@@ -103,29 +223,30 @@ func workerPool(in <-chan string, numWorkers int) <-chan string {
 }
 
 func main() {
-	urls := []string{"https://example.com", "https://golang.org", "https://reddit.com"}
+	entryURLs := []string{"https://en.wikipedia.org/wiki/Main_Page"}
 
-	// Generator pattern
-	urlChannel := generateURLs(urls...)
+	// Generate URLs from entry URLs
+	urlChannel := generateURLs(entryURLs...)
 
-	// Fan-out pattern
-	numWorkers := 3
+	// Fetch HTML content from URLs
+	numWorkers := runtime.NumCPU()
 	fetchedChannels := make([]<-chan string, numWorkers)
 	for i := 0; i < numWorkers; i++ {
 		fetchedChannels[i] = fetchURL(urlChannel)
 	}
 
-	// Fan-in pattern
+	// Collect and merge HTML content from multiple channels
 	processedHTML := processResponses(fetchedChannels)
 
-	// Pipeline pattern
+	// Process HTML content
 	processedData := processHTML(processedHTML)
 
-	// Worker pool pattern
+	// Worker pool for processing HTML content
 	numWorkerPool := 2
 	finalResult := workerPool(processedData, numWorkerPool)
 
+	// Output
 	for res := range finalResult {
-		fmt.Println(res)
+		log.Println(res)
 	}
 }
